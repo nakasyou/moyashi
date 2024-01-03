03935f940f8b3bf19bf31011f1a622100d640a42
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-01-03 11:32:32 +0000

    feat: add methods

--- src/types/datas.ts
+++ src/types/datas.ts
@@ -1,2 +1,8 @@
-export const methods = ['GET', 'POST', 'PUT'] as const
+export const methods = [
+  'GET',
+  'POST',
+  'PUT',
+  'OPTIONS',
+  'DELETE',
+] as const
 export type Method = typeof methods[number]
